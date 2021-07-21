6164db5db053c2a67d959b5b7b95381021368e85
Victor <viktor.vidovski@gmail.com>
2021-07-21 23:15:23 +0200

    and onwards

---
+++ mail/mail/static/mail/inbox.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-form').addEventListener('onsubmit', function() {
+
+    var compdata = {
+      'recipients': document.querySelector('#compose-recipients').value,
+      'subject': document.querySelector('#compose-subject').value,
+      'body': document.querySelector('#compose-body').value
+    }
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    fetch('/emails', {
+      method: 'POST',
+      headers: {'X-CSRFToken': csrftoken}
+      mode: 'same-origin'
+    })
+    .then(response => response.json())
+  });
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
